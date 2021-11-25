a67b9eef6418a1509b6ace71aea97ac7452435ba
Gerardo Merida <cgemerida@gmail.com>
2021-11-24 21:49:47 -0600

    Jest/Jaszmine basic testing

--- testing/react-basics/src/App.test.js
+++ testing/react-basics/src/App.test.js
@@ -1,3 +1,4 @@
+/* leave first line blank for cq */
 import App from './App';
 import React from 'react';
 import { shallow } from 'enzyme';
@@ -6,28 +7,49 @@ describe('App', () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<App />);
+    wrapper = shallow(
+      <App />
+    );
   });
 
   it('should have the `th` "Items"', () => {
-    expect(wrapper.contains(<th>Items</th>)).toBe(true);
+    expect(
+      wrapper.contains(<th>Items</th>)
+    ).toBe(true);
   });
 
   it('should have a `button` element', () => {
-    expect(wrapper.containsMatchingElement(<button>Add item</button>)).toBe(true);
+    // Commented part also works
+    // const btn = wrapper.find('button').first();
+    // expect(
+    //   btn !== null
+    // ).toBe(true);
+    expect(
+      wrapper.containsMatchingElement(
+        <button>Add item</button>
+      )
+    ).toBe(true);
+  });
+
+  it('should have an `input` element', () => {
+    expect(
+      wrapper.containsMatchingElement(
+        <input />
+      )
+    ).toBe(true);
   });
 
   it('`button` should be disabled', () => {
     const button = wrapper.find('button').first();
-    expect(button.props().disabled).toBe(true);
+    expect(
+      button.props().disabled
+    ).toBe(true);
   });
 
-  it('should have an `input` element', () => {
-    expect(wrapper.containsMatchingElement(<input />)).toBe(true);
-  });
 
+  // Behavior-driven test
   describe('the user populates the input', () => {
-    const item = 'London';
+    const item = 'Vancouver';
 
     beforeEach(() => {
       const input = wrapper.find('input').first();
@@ -37,12 +59,16 @@ describe('App', () => {
     });
 
     it('should update the state property `item`', () => {
-      expect(wrapper.state().item).toEqual(item);
+      expect(
+        wrapper.state().item
+      ).toEqual(item);
     });
 
     it('should enable `button`', () => {
       const button = wrapper.find('button').first();
-      expect(button.props().disabled).toBe(false);
+      expect(
+        button.props().disabled
+      ).toBe(false);
     });
 
     describe('and then clears the input', () => {
@@ -55,7 +81,9 @@ describe('App', () => {
 
       it('should disable `button`', () => {
         const button = wrapper.find('button').first();
-        expect(button.props().disabled).toBe(true);
+        expect(
+          button.props().disabled
+        ).toBe(true);
       });
     });
 
@@ -63,27 +91,47 @@ describe('App', () => {
       beforeEach(() => {
         const form = wrapper.find('form').first();
         form.simulate('submit', {
-          preventDefault: () => {},
+          preventDefault: () => { }
         });
       });
 
-      it('should add the item to state', () => {
-        expect(wrapper.state().items).toContain(item);
+      it('should add the item to state `items`', () => {
+        expect(
+          wrapper.state().items
+        ).toContain(item);
       });
 
-      it('should render the item in the table', () => {
-        expect(wrapper.containsMatchingElement(<td>{item}</td>)).toBe(true);
+      it('should add the item in the table', () => {
+        // This is my way
+        // const td = wrapper.find('td').last();
+        // expect(
+        //   td.containsMatchingElement(<td>{item}</td>)
+        // ).toBe(true);
+
+        expect(
+          wrapper.containsMatchingElement(<td>{item}</td>)
+        ).toBe(true);
       });
 
-      it('should clear the input field', () => {
+      it('should clear the input', () => {
+        // My solution
+        // expect(
+        //   wrapper.state().item
+        // ).toBe('');
+
         const input = wrapper.find('input').first();
-        expect(input.props().value).toEqual('');
+        expect(
+          input.props().value
+        ).toEqual('')
       });
 
       it('should disable `button`', () => {
         const button = wrapper.find('button').first();
-        expect(button.props().disabled).toBe(true);
+        expect(
+          button.props().disabled
+        ).toBe(true);
       });
     });
+
   });
 });
