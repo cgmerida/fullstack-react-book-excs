a981a6b47919bb17d61f4d91b9aeaaaaf3927dae
Gerardo Merida <cgemerida@gmail.com>
2021-11-26 19:44:09 -0600

    Routing basics

--- routing/basics/src/App.js
+++ routing/basics/src/App.js
@@ -1,35 +1,13 @@
-import React from 'react';
+import React, { useEffect, useReducer } from 'react';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div
-        className='ui text container'
-      >
-        <h2 className='ui dividing header'>
-          Which body of water?
-        </h2>
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  Switch,
+} from 'react-router-dom'
 
-        <ul>
-          <li>
-            <a href='/atlantic'>
-              <code>/atlantic</code>
-            </a>
-          </li>
-          <li>
-            <a href='/pacific'>
-              <code>/pacific</code>
-            </a>
-          </li>
-        </ul>
-
-        <hr />
-
-        {/* We'll insert the Route components here */}
-      </div>
-    );
-  }
-}
 
 const Atlantic = () => (
   <div>
@@ -49,6 +27,92 @@ const Pacific = () => (
       'mar pacifico' in 1521, which means peaceful sea.
     </p>
   </div>
+);
+
+const BlackSea = () => {
+  const [count, countDecrement] = useReducer(c => c - 1, 3)
+
+  useEffect(() => {
+    const intID = setInterval(() => {
+      countDecrement()
+    }, 1000);
+    return () => {
+      if (intID)
+        clearInterval(intID)
+    }
+  }, [])
+
+  return (
+    <div>
+      <h3>Black Sea</h3>
+      <p>Nothing to sea [sic] here ...</p>
+      <p>Redirecting in {count}...</p>
+      {
+        (count < 1) ? (
+          <Redirect to='/' />
+        ) : null
+      }
+    </div>
+  );
+}
+
+const App = () => (
+  <Router>
+    <div
+      className='ui text container'
+    >
+      <h2 className='ui dividing header'>
+        Which body of water?
+      </h2>
+
+      <ul>
+        <li>
+          <Link to='/atlantic'>
+            <code>/atlantic</code>
+          </Link>
+        </li>
+        <li>
+          <Link to='/pacific'>
+            <code>/pacific</code>
+          </Link>
+        </li>
+        <li>
+          <Link to='/black-sea'>
+            <code>/black-sea</code>
+          </Link>
+        </li>
+      </ul>
+
+      <hr />
+      <Switch>
+        <Route path='/atlantic/ocean' render={() => (
+          <div>
+            <h3>Atlantic Ocean — Again!</h3>
+            <p>
+              Also known as "The Pond."
+            </p>
+          </div>
+        )} />
+        <Route path='/atlantic' component={Atlantic} />
+        <Route path='/pacific' component={Pacific} />
+        <Route path='/black-sea' component={BlackSea} />
+
+        <Route exact path='/' render={() => (
+          <h3>
+            Welcome! Select a body of saline water above.
+          </h3>
+        )} />
+
+        <Route render={({ location }) => (
+          <div className='ui inverted red segment'>
+            <h3>
+              Error! No matches for <code>{location.pathname}</code>
+            </h3>
+          </div>
+        )} />
+      </Switch>
+    </div>
+  </Router>
 );
 
 export default App;
